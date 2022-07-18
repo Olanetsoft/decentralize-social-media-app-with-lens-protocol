f2838cd2790699cfde0789a9ddd648c22fe96f43
Idris Olubisi <heedris2olubisi@gmail.com>
2022-07-19 00:42:40 +0100

    Starter

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
 import Head from "next/head";
-import Profiles from "../components/profiles";
 
-import { useState, useEffect } from "react";
-
-import { client, getProfiles } from "../pages/api/api";
+import Image from "next/image";
 
 export default function Home() {
-  const [profiles, setProfiles] = useState([]);
-
-  const fetchProfiles = async () => {
-    try {
-      const response = await client.query(getProfiles).toPromise();
-
-      setProfiles(response.data.recommendedProfiles);
-
-      console.log(response.data.recommendedProfiles);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  useEffect(() => {
-    fetchProfiles();
-  }, []);
-
   return (
     <div className="grid grid-cols-3 divide-x">
       <Head>
@@ -41,13 +20,34 @@ export default function Home() {
           </p>
         </div>
       </div>
-      {profiles && profiles.length > 0 ? (
-        <Profiles profiles={profiles} />
-      ) : (
-        <div className="text-center text-gray-500 p-5 font-medium text-xl tracking-tight leading-tight">
-          <p>Loading...</p>
+      <div className="max-w-md rounded-lg shadow-lg bg-white mt-5 mb-5 p-5 border border-radius-8 cursor-pointer hover:bg-gray-100 hover:shadow-lg ml-8">
+        <div className="flex flex-shrink-0 p-4 pb-0">
+          <div className="flex items-center">
+            <div>
+              <Image
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/195612/tt_avatar_tailwind.jpg"
+                alt="Olanetsoft"
+                width={64}
+                height={64}
+                className="rounded-full"
+              />
+            </div>
+            <div className="ml-3">
+              <p className="text-base leading-6 font-medium ">
+                I D R I S{" "}
+                <span className="text-sm leading-5 font-medium text-gray-500 group-hover:text-gray-400 transition ease-in-out duration-150">
+                  @olanetsoft.lens
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-      )}
+        <div className="pl-16">
+          <p className="text-base width-auto font-small flex-shrink">
+            A software engineer, technical writer, and community builder.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
