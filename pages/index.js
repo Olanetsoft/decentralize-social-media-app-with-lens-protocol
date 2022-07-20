94ce368fcae1bf35e513eb94ea0dabe393742041
Idris Olubisi <heedris2olubisi@gmail.com>
2022-07-21 00:45:53 +0100

    Ready to ship

--- pages/index.js
+++ pages/index.js
@@ -31,17 +31,17 @@ export default function Home() {
   return (
     <div className="grid grid-cols-3 divide-x">
       <Head>
-        <title>Decentralize Social Media App - Lens protocol</title>
+        <title>Decentralized Social Network - Lens protocol</title>
         <meta name="description" content="Decentralize Social Media App" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="col-span-3">
         <div className="px-4 py-8">
           <h1 className="text-3xl font-bold leading-tight text-center">
-            Decentralize Social Media App - Lens protocol
+            Decentralized Social Network - Lens protocol
           </h1>
           <p className="text-center">
-            This is a decentralized social media app built on the Lens protocol.
+            This is a decentralized social network built on the Lens protocol.
           </p>
         </div>
       </div>
